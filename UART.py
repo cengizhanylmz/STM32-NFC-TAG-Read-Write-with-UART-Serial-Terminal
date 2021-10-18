a4d7fbcab092ceb5791f114dedca751633dedcc3
cengizhanylmz <71149855+cengizhanylmz@users.noreply.github.com>
2021-10-18 11:42:39 +0300

    Update UART.py

--- UART.py
+++ UART.py
@@ -1,16 +1,23 @@
 import sys
-import serial,serial.tools.list_ports
+import sqlite3
 from PyQt5 import QtWidgets,QtSerialPort
 from PyQt5.QtGui import QPalette
 from PyQt5.QtCore import Qt,QIODevice,QTimer
 import time
+import datetime
+import locale
+import os
+
+class Window2(QtWidgets.QWidget):
+    def __init__(self):
+        super(Window2, self).__init__()
+        self.setWindowTitle("Pencere")
 
 class Window(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
+        locale.setlocale(locale.LC_ALL,"")
         self.unit_ui()
-
 
     def unit_ui(self):
         self.text_area=QtWidgets.QTextEdit()
@@ -20,12 +27,14 @@ class Window(QtWidgets.QWidget):
         self.stop=QtWidgets.QPushButton("Stop Bits")
         self.clear=QtWidgets.QPushButton("Clear")
         self.send=QtWidgets.QPushButton("Send Data")
-        self.send.setShortcut("Enter")
         self.connection=QtWidgets.QPushButton("Connect")
         self.disconnect=QtWidgets.QPushButton("Disconnect")
         self.lineEdit=QtWidgets.QLineEdit()
         self.nfcsend=QtWidgets.QPushButton("Write A Record to NFC")
         self.nfclineEdit=QtWidgets.QLineEdit()
+        self.writeFile=QtWidgets.QPushButton("Open Data Base")
+        self.clearFile=QtWidgets.QPushButton("Clear Data Base")
+        self.database=QtWidgets.QPushButton("Data Base")
 
 
         self.v_box=QtWidgets.QVBoxLayout()
@@ -43,6 +52,7 @@ class Window(QtWidgets.QWidget):
         self.h_box.addWidget(self.connection)
         self.h_box.addWidget(self.disconnect)
         self.h_box.addWidget(self.clear)
+        self.h_box.addWidget(self.database)
 
         self.h1_box.addStretch(1)
 
@@ -60,6 +70,7 @@ class Window(QtWidgets.QWidget):
         self.setLayout(self.v_box)
         self.clear.clicked.connect(self.clear_text)
 
+
     def clear_text(self):
         self.text_area.clear()
 
@@ -70,6 +81,15 @@ class Work(QtWidgets.QMainWindow):
         self.qserial =QtSerialPort.QSerialPort()
         self.setCentralWidget(self.window)
         self.menus()
+        self.baglanti_Olustur()
+
+    def baglanti_Olustur(self):
+        self.baglanti=sqlite3.connect("data_db")
+        self.cursor=self.baglanti.cursor()
+
+        self.cursor.execute("Create Table If not exists DATA(TIME TEXT,TEXT TEXT, URL TEXT)")
+
+        self.baglanti.commit()
     def menus(self):
         self.serial_boud=None
         self.serial_data=None
@@ -79,10 +99,23 @@ class Work(QtWidgets.QMainWindow):
         menu_boud=QtWidgets.QMenu()
         menu_data=QtWidgets.QMenu()
         menu_stop=QtWidgets.QMenu()
+        menu_file=QtWidgets.QMenu()
         self.window.coms.setMenu(menu_coms)
         self.window.bound.setMenu(menu_boud)
         self.window.data.setMenu(menu_data)
         self.window.stop.setMenu(menu_stop)
+        self.window.database.setMenu(menu_file)
+
+
+        readdata=QtWidgets.QAction("Open Data Base",self)
+        cleardata=QtWidgets.QAction("Clear Data Base",self)
+        updatedata=QtWidgets.QAction("Update Data Base",self)
+
+        menu_file.addAction(readdata)
+        menu_file.addAction(cleardata)
+        menu_file.addAction(updatedata)
+
+
         ## COMS PARAMETERS ##
         com3=QtWidgets.QAction("COM4",self)
         com4 = QtWidgets.QAction("COM5", self)
@@ -136,12 +169,22 @@ class Work(QtWidgets.QMainWindow):
         menu_stop.triggered.connect(self.response_stop)
         self.window.connection.clicked.connect(self.connection)
         self.window.disconnect.clicked.connect(self.disconnection)
+        self.window.writeFile.clicked.connect(self.window2)
+        self.window.clearFile.clicked.connect(self.clearFile)
+        menu_file.triggered.connect(self.response_database)
 
 
         self.window.nfcsend.clicked.connect(self.send_NFC_data)
 
         self.setWindowTitle("UART TERMINAL")
         self.setGeometry(600, 200, 625, 625)
+    def response_database(self,action_sqlt):
+        if action_sqlt.text()=="Open Data Base":
+            self.window2()
+        elif action_sqlt.text()=="Clear Data Base":
+            self.clearFile()
+        elif action_sqlt.text()=="Update Data Base":
+            self.updateFile()
     def response_com(self,action):
 
         self.serial_port=action.text()
@@ -226,9 +269,38 @@ class Work(QtWidgets.QMainWindow):
 
         self.qserial.write(self.window.nfclineEdit.text().encode("utf-8"))
         self.window.text_area.append("Data to be written to the tag: {} ".format(self.window.nfclineEdit.text()))
+        data=self.window.nfclineEdit.text()
+
+        now=datetime.datetime.now()
+        a=datetime.datetime.strftime(now,"%d %B %Y %X")
+
+        a=str(a)
+
         self.window.nfclineEdit.clear()
+        self.cursor.execute("Insert into DATA Values(?,?,'NoNe')", (a, data))
+        self.baglanti.commit()
 
+    def window2(self):
+        #dosya_ismi = QtWidgets.QFileDialog.getOpenFileName(self, "Open", os.getenv("HOME"))
+        #print(dosya_ismi)
+        #with open(dosya_ismi[0], "r") as file:
+            #self.text_area.append(file.read())
+        self.cursor.execute("Select * From DATA")
+        rows=self.cursor.fetchall()
+        if len(rows)==0:
+            self.window.text_area.append("Data Base is empty!")
+        else:
+            self.window.text_area.append("DATE                                      DATA")
+            self.window.text_area.append("¯¯¯¯¯¯¯                                  ¯¯¯¯¯¯¯\n ")
+            for row in rows:
+                self.window.text_area.append("{}             {}".format(row[0],row[1]))
 
+                self.window.text_area.append("\n")
+    def clearFile(self):
+        self.cursor.execute("Delete From DATA")
+        self.baglanti.commit()
+    def updateFile(self):
+        pass
 app = QtWidgets.QApplication(sys.argv)
 app.setStyle('Fusion')
 palet = QPalette()
